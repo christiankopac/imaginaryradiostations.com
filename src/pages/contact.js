e09dbe40aa96dc87bcc8354a3cb6c1ef743edfe3
Christian Kopac <hello@christiankopac.com>
2017-11-14 02:58:45 +0100

    Updated name and added social links

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -4,38 +4,6 @@ import AnimatedText from '../components/AnimatedText/AnimatedText.js'
 import SubscribeFrom from "react-mailchimp-subscribe"
 import Link from 'gatsby-link'
 
-const formProps = {
-  action: "https://sloarg.us2.list-manage.com/subscribe/post?u=9ad0f510b205557f49b6e903e&amp;id=d63a1f66ac",
-  messages: {
-    inputPlaceholder: "email",
-    btnLabel: "Subscribe",
-    sending: "Sending...",
-    success: "Danke!!",
-    error: "Perdón! Impossible to subscribre that address"
-  },
-  styles: {
-    btnLabel: {
-      marginLeft: "40"
-    },
-    inputPlaceholder: {
-      marginRight: "40px"
-    },
-    sending: {
-      fontSize: 18,
-      color: "auto"
-    },
-    success: {
-      fontSize: 18,
-      color: "green"
-    },
-    error: {
-      fontSize: 18,
-      color: "red"
-    }
-  }
-}
-const Form = () => <SubscribeFrom {...formProps} />;
-
 export default () =>
   <div style={{ margin: '3rem auto', maxWidth: 600 }}>
     <h1>Get in touch!</h1>
@@ -43,8 +11,6 @@ export default () =>
       <p>
       Write us on <a href="https://www.facebook.com/pg/Imaginary-Radio-Stations-723683261161286">facebook</a> or send us an email to <a href="mailto:yes@imaginaryradiostations.com">yes@imaginaryradiostations.com</a>
       </p>
-      <Form />
     </div>
-    <AnimatedText text="imaginary radio stations"/>
     <Logo />
   </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
-import React from 'react'
-import Logo from '../components/Logo/Logo.jsx'
+import React from 'react';
+import Logo from '../components/Logo/Logo.jsx';
 
-export default () =>
-    <Logo />
+
+export default () =>  
+    <Logo />;
