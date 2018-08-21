7c8bda04594ec84fdf6ca03b3574d1c20b6e18d5
Christian Kopac <christian.kopac@gmail.com>
2018-08-21 03:50:41 +0200

    wow. such productivity

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import Logo from '../components/Logo/Logo.jsx';
+import Link from "gatsby-link"
+import flyer from './irs-7.jpg'
 
-
-export default () =>  
-    <Logo />;
+export default () =>
+    <div>
+        <Link to="/events/">Next event 24.8 @ Maze</Link>
+        <img src={flyer} alt="24.8 IRS #7 @ MAZE"/>
+        <Logo />;
+    </div>
