ad5520f9fd27ecebbe8bf2d438deb3ba30d9f101
Christian Kopač <christian.kopac@gmail.com>
2018-08-29 00:46:42 +0200

    Added mix to home

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,6 +12,9 @@ const Index = styled.div`
 export default () =>
     <Index>
         {/* <Link to="/events/">Next event 24.8 @ Maze</Link> */}
-        <a href=""><img src={flyer} alt="24.8 IRS #7 @ MAZE"/></a>
+        <a href=""><img src={flyer} alt="24.8 IRS #7 @ MAZE" /></a>
+
+        <iframe width="100%" height="20" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/491800074&color=%23443c44&inverse=false&auto_play=true&show_user=true"></iframe>
+
         {/* <Logo />; */}
     </Index>
