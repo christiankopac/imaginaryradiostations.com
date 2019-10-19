509bab81a81acf98b4382c0be9c6d267e997b2c9
Christian Kopač <christian@carfellows.com>
2019-10-19 13:05:27 +0200

    code refactoring

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,55 +1,3 @@
-// const path = require('path')
-// const { createFilePath } = require('gatsby-source-filesystem')
-//
-// // Log out information after a build is done
-// exports.onPostBuild = ({ reporter }) => {
-//   reporter.info('Your Gatsby site has been built!')
-// }
-// exports.onCreateNode = ({ node, actions, getNode }) => {
-//   const { createNodeField } = actions
-//   if (node.internal.type === 'MarkdownRemark') {
-//     const slug = createFilePath({ node, getNode })
-//     createNodeField({
-//       node,
-//       name: 'slug',
-//     })
-//     const audio = createFilePath({ node, getNode })
-//     createNodeField({
-//       node,
-//       name: 'audio'
-//     })
-//   }
-// }
-//
-// exports.createPages = ({ graphql, actions }) => {
-//   const { createPage } = actions
-//   return new Promise((resolve) => {
-//     graphql(`
-//       {
-//         allMarkdownRemark {
-//           edges {
-//             node {
-//               fields {
-//                 slug
-//                 audio
-//               }
-//             }
-//           }
-//         }
-//       }
-//     `).then((result) => {
-//       result.data.allMarkdownRemark.edges.map(({ node }) => createPage({
-//         path: node.fields.slug,
-//         component: path.resolve('./src/templates/mix.jsx'),
-//         context: {
-//           slug: node.fields.slug,
-//         },
-//       }))
-//       resolve()
-//     })
-//   })
-// }
-
 const { createFilePath } = require('gatsby-source-filesystem')
 
 exports.onCreateNode = ({ node, getNode, actions }) => {
@@ -64,25 +12,7 @@ exports.onCreateNode = ({ node, getNode, actions }) => {
     createNodeField({
       node,
       name: 'audio',
-      value: audio,
     })
   }
 }
-const axios = require("axios")
-const get = endpoint => axios.get(`https://pokeapi.co/api/v2${endpoint}`)
-const getPokemonData = names =>
-  Promise.all(
-    names.map(async name => {
-      const { data: pokemon } = await get(`/pokemon/${name}`)
-      return { ...pokemon }
-    })
-  )
-exports.createPages = async ({ actions: { createPage } }) => {
-  const allPokemon = await getPokemonData(["pikachu", "charizard", "squirtle"])
-  // Create a page that lists Pokémon.
-  createPage({
-    path: `/`,
-    component: require.resolve("./src/templates/all-pokemon.js"),
-    context: { allPokemon },
-  })
-}
+
